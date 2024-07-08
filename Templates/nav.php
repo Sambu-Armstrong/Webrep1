1918b4a4bcfcbe1c0ca9a4bde3e10c450def1986
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-08 13:20:28 +0300

    added  home, login,contact

---
+++ Templates/nav.php
@@ -0,0 +1,12 @@
+<header>
+    <h2 class="logo">Ent District</h2>
+    <nav class="navigation">
+        <a href="./">Home</a>
+        <a href="#">Books</a>
+        <a href="#">Movies</a>
+        <a href="#">About</a>
+        <a href="contact.html">Contact</a>
+        <a href="login.html">Login</a>
+
+    </nav>
+</header>

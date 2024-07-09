6addf452bf4faaf137c711a3db0c067987eb8746
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-09 22:35:01 +0300

    removed cssFile

--- Templates/header.php
+++ Templates/header.php
@@ -5,6 +5,6 @@
     <meta http-equiv="X-UA_Compatible" content="IE=e">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>

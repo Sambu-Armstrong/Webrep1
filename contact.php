21d2286d034c5fbb863e3522166c557768f28377
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-09 23:06:32 +0300

    changing to php

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<body style="background: url('Images/avenue.jpg') no-repeat ;
+            background-size: cover ;
+            background-position: center;">
+            
+    <div class="wrapper">
+    <form action="">
+        <h1>Contact us</h1>
+        <div class = "input-box">
+            <input type = "text" placeholder = "First name " required>
+            
+        </div>
+        <div class = "input-box">
+            <input type = "text" placeholder = "Last name " required>
+        </div>
+        <div class = "input-box">
+            <input type = "email" placeholder = "Email " required>
+            
+        </div>
+        <div class="tarea">
+            <textarea name="Message" class="from-control" placeholder="Write here ..." required></textarea>
+        </div>
+        
+        <button type="submit" class="btn">Send</button>
+        
+    </form>
+    </div>
+</body>
+</html>

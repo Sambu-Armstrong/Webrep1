eb8c789849d92f4c3142e0d06498be480e27a699
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-10 22:30:53 +0300

    adding text

--- index.php
+++ index.php
@@ -2,13 +2,39 @@
     include_once("Templates/header.php");
     include_once("Templates/nav.php");
     include_once("Templates/background.php");
+    
 ?>
 
 <body >
-    
-        
+    <div style = "
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    text-align: center;
+    color: #fff;
+    ">
 
-        
+    <h1 style = "
+        text-align: centre;
+        color: #fff;
+        ">DISCOVER YOUR TASTE</h1>
+        <P style = "
+            margin: 20px auto;
+    font-weight: 100;
+    line-height: 20px;
+        "> Easily find your next watch/read and stop wasting 
+            more <br>time to find your taste
+        </P>
+        <div>
+            <a href="about.php">
+                <button type="button">Learn more</button>
+            </a>   
+            <a href = "login.php"> 
+                <button type = "button">Login</button>
+            </a>
+        </div>
     </div>
+        
 </body>
 </html>

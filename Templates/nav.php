d9f70020243e241c84eeb9611611264555f9ace4
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-10 15:31:29 +0300

    books to php

--- Templates/nav.php
+++ Templates/nav.php
@@ -2,7 +2,7 @@
     <h2 class="logo">Ent District</h2>
     <nav class="navigation">
         <a href="./">Home</a>
-        <a href="books.html">Books</a>
+        <a href="books.php">Books</a>
         <a href="movies.html">Movies</a>
         <a href="#">About</a>
         <a href="contact.php">Contact</a>

5646082505ad308252976ed2253676b298177a3a
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-10 14:46:58 +0300

    background image

---
+++ Templates/background.php
@@ -0,0 +1,14 @@
+<style>
+        body {
+            display: flex;
+            justify-content:center;
+            margin-top: 70px;
+            background: url(Images/avenue.jpg) no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+        .image-row h1{
+            color: #6b65bc;
+        }
+       
+    </style>

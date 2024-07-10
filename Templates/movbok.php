475a9f6c72de5685d624f86c2148a39661f45004
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-10 21:12:33 +0300

    adding movies/books style

---
+++ Templates/movbok.php
@@ -0,0 +1,22 @@
+<style>
+        body {
+            display:grid;
+            justify-content:center;
+            margin-top: 80px;
+            background: url(Images/avenue.jpg) no-repeat;
+            background-size: cover;
+            background-position: center;
+            align-items:safe;
+            min-height: 80vh;
+        }
+        .image-row h1{
+            color: #d1bca4;
+            text-decoration: underline;
+            text-align: center;
+            margin: 3%;
+        }
+        .image-row a{
+            margin: 10px;
+        }
+       
+    </style>

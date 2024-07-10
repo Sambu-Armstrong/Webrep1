10d10b253cd231b0056f678fe8838f164900d913
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-10 15:22:38 +0300

    changing to php

---
+++ books.php
@@ -0,0 +1,32 @@
+<?php
+    
+?>
+</head>
+<body >
+    
+
+    <div class="image-row">
+        <h1>Book Recommendation</h1>
+        <a href="https://www.goodreads.com/book/show/5907.The_Hobbit" target="_blank">
+            <img src="Images/The Hobbit.jpg" width="200" height="200" title="Lord of the Rings: The Hobbit" alt="Lord of the Rings: The Hobbit">
+        </a>
+        &nbsp&nbsp&nbsp
+        <a href="https://www.goodreads.com/book/show/7205214-renaissance" target="_blank">
+            <img src="Images/Renaisance.jpg" width="200" height="200" title="Assasins Creed: Renaissance" alt="Assasins Creed: Renaissance">
+        </a>
+        &nbsp&nbsp&nbsp
+        <a href="https://www.goodreads.com/book/show/13555073-the-screaming-staircase" target="_blank">
+            <img src="Images/Lockwood_ Screaming.jpeg" width="200" height="200" title="Lockwood & Co: The Screaming Staircase" alt="Lockwood & Co: The Screaming Staircase">
+        </a>
+        &nbsp&nbsp&nbsp
+        <a href="https://www.gutenberg.org/ebooks/41537" target="_blank">
+            <img src="Images/Divine Comedy_Inferno.jpeg" width="200" height="200" title="Dante's Divine Comedy: Inferno" alt="Dante's Divine Comedy: Inferno">
+        </a>
+        &nbsp&nbsp&nbsp
+        <a href="https://www.goodreads.com/work/quotes/1950213-onze-minutos" target="_blank">
+            <img src="Images/eleven minutes_Paulo Coehlo.jpeg" width="200" height="200" title="Eleven Minutes" alt="Eleven Minutes by Paulo Coehlo">
+        </a>
+        &nbsp&nbsp&nbsp
+    </div>
+</body>
+</html>

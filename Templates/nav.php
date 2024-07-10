806ee3d711bfab486e912afa08636c56ed166066
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-10 14:44:32 +0300

    adding books link

--- Templates/nav.php
+++ Templates/nav.php
@@ -2,10 +2,10 @@
     <h2 class="logo">Ent District</h2>
     <nav class="navigation">
         <a href="./">Home</a>
-        <a href="#">Books</a>
-        <a href="#">Movies</a>
+        <a href="books.html">Books</a>
+        <a href="movies.html">Movies</a>
         <a href="#">About</a>
-        <a href="contact.html">Contact</a>
+        <a href="contact.php">Contact</a>
         <a href="login.php">Login</a>
 
     </nav>

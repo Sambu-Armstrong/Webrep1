ebfcd0c03e6a4b570aaba0da8808d80fdf2b1220
Armstrong-S <armstrong.sambu@strathmore.edu>
2024-07-11 15:04:58 +0300

    adding database name

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $db_name = "";
+    $db_name = "webdb";
 
     try{
         $conn = new mysqli($servername, $username, $password, $db_name);
